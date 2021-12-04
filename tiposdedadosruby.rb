baf108f64c25939d1d272edc5fd8d4edd1b03f2a
Gustavo Trajano <gustavotrajano3@gmail.com>
2021-12-04 14:54:04 -0300

    Initial Process

---
+++ tiposdedadosruby.rb
@@ -0,0 +1,11 @@
+nome = "Gustavo"
+//texto
+idade = 18
+//numero
+gpa = 3.2
+//decimal
+ehhomem = true
+//booleano
+ehalto = false
+
+emprego = "programador"
---
+++ variaveisruby.rb
@@ -0,0 +1,12 @@
+nome_personagem = "clar"
+idade_personagem = "50"
+puts "era uma vez"
+puts ("um homem chamado " + nome_personagem + "tem" + idade_personagem + " Anos")
+idade_personagem = "35"
+puts ("o homem tinha a idade" + idade_personagem) 
+puts ("e não gostava de ter " + idade_personagem + "anos")
+
+
+
+
+
